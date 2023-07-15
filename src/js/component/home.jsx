f346d15b5546285b94ee9c84faa10fdfabcf9bac
nick2918 <35611616+nick2918@users.noreply.github.com>
2023-07-15 21:29:01 +0000

    prueba icono

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = (props) => {
 
 	return (
-		<div className="d-inline-flex flex-rowtext-center">
-			<p></p>
+		<div className="d-inline-flex flex-rowtext-center text-center position-absolute top-0 start-50 translate-middle-x">
+			<Icono/>
 			<p className="p-2">{props.cienMiles}</p>
 			<p className="p-2">{props.diezMiles}</p>
 			<p className="p-2">{props.miles}</p>
---
+++ src/js/component/reloj.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Icono(){
+    <div>
+       <p><i className="bi bi-clock"></i></p> 
+    </div>
+}
+
+export default Icono;
